ad610ebb2ceb6f49e50f81bf56e6916c049cc399
Sudhanandhini <sudhanandhinis@gmail.com>
2025-09-18 12:06:40 +0530

    1st commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://childfund-onlinetest.onrender.com';
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
 
 // Create axios instance with default config
 const api = axios.create({
